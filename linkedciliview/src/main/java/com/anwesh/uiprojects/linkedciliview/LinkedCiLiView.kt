27694b201aaa7a97e2a87145835c6b87e095b209
Anwesh43 <anweshthecool0@gmail.com>
2018-07-06 10:10:18 +0530

    adding linkedliciview module

---
+++ linkedciliview/src/main/java/com/anwesh/uiprojects/linkedciliview/LinkedCiLiView.kt
@@ -0,0 +1,11 @@
+package com.anwesh.uiprojects.linkedciliview
+
+/**
+ * Created by anweshmishra on 06/07/18.
+ */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint 
