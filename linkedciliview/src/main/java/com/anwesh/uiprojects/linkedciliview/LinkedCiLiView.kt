c4f3262bf0f179fe58aee70df3d16ffeb257211b
Anwesh43 <anweshthecool0@gmail.com>
2018-07-06 10:57:12 +0530

    adding LinkedLICI class

--- linkedciliview/src/main/java/com/anwesh/uiprojects/linkedciliview/LinkedCiLiView.kt
+++ linkedciliview/src/main/java/com/anwesh/uiprojects/linkedciliview/LinkedCiLiView.kt
@@ -131,4 +131,28 @@ class LinkedCiLiView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedLICI(var i : Int) {
+
+        private var curr : LICINode = LICINode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                stopcb(it)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            curr.startUpdating(startcb)
+        }
+    }
 }
