1dab18bfb97a7cd859a707e4d1568fd7ee7ba03f
Anwesh43 <anweshthecool0@gmail.com>
2018-07-06 11:10:13 +0530

    adding static create method

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedciliview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedciliview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedciliview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedciliview.LinkedCiLiView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedCiLiView.create(this)
     }
 }
