e246d43b1355a2f00497c982664e8900095886af
Anwesh43 <anweshthecool0@gmail.com>
2018-07-06 10:14:50 +0530

    adding LinkedLiCiView class

--- linkedciliview/src/main/java/com/anwesh/uiprojects/linkedciliview/LinkedCiLiView.kt
+++ linkedciliview/src/main/java/com/anwesh/uiprojects/linkedciliview/LinkedCiLiView.kt
@@ -8,4 +8,22 @@ import android.content.Context
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Canvas
-import android.graphics.Paint 
+import android.graphics.Paint
+
+val LI_CI_NODES : Int = 5
+
+class LinkedCiLiView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
